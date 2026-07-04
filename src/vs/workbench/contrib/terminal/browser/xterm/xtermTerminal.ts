a280122df4fc0d2dd97ae8f163b152113c9de9f3
Z User <z@container>
2026-07-04 12:06:15 +0000

    fix: remove deprecated fastScrollModifier option (xterm beta removed it)

--- src/vs/workbench/contrib/terminal/browser/xterm/xtermTerminal.ts
+++ src/vs/workbench/contrib/terminal/browser/xterm/xtermTerminal.ts
@@ -213,7 +213,6 @@ export class XtermTerminal extends Disposable implements IXtermTerminal, IDetach
                         macOptionIsMeta: config.macOptionIsMeta,
                         macOptionClickForcesSelection: config.macOptionClickForcesSelection,
                         rightClickSelectsWord: config.rightClickBehavior === 'selectWord',
-                        fastScrollModifier: 'alt' as any,
                         fastScrollSensitivity: config.fastScrollSensitivity,
                         scrollSensitivity: config.mouseWheelScrollSensitivity,
                         wordSeparator: config.wordSeparators,
